6912e8f02f4b640d33988dab52f5e6473e9bb470
HUAWEI PYCHARM <dearhaly@163.com>
2022-11-17 16:02:33 +0800

    2022/11/17/ HUAWEI
    Signed-off-by: HUAWEI PYCHARM <dearhaly@163.com>

--- 套接字/test.py
+++ 套接字/test.py
@@ -1,1 +1,29 @@
-print("test")
+import threading
+import time
+
+lock = threading.Lock()
+
+def targetFunc(threadNum):
+    # lock.acquire()
+    # time.sleep(1)
+    print(f"this is thread:{threadNum}".format(threadNum))
+    time.sleep(2)
+    print(f"thread: {threadNum} sleep over!".format(threadNum=threadNum))
+    lock.acquire()
+    lock.release()
+    return True
+
+def mainThread():
+    threadObjList = []
+    threadArgList = list(i for i in range(100))
+
+    for i in range(100):
+        # print("i")
+        tmpThreadObject = threading.Thread(target=targetFunc, args=(threadArgList[i],))
+        threadObjList.append(tmpThreadObject)
+    for i in range(len(threadObjList)):
+        obj = threadObjList[i]
+        obj.start()
+        # obj.join()
+
+mainThread()
